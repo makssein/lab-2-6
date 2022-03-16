e740c81a6a3616b4be5186aa61753b6445322cd5
deekep <MVSein@stud.kantiana.ru>
2022-03-16 23:08:54 +0200

    add vector and binarySearch

--- main.cpp
+++ main.cpp
@@ -1,40 +1,70 @@
 #include <iostream>
-#include <list>
+#include <vector>
 
-void quicksort(int a, int b, int* x) {
+void quickSort(int a, int b, std::vector<int>& mas) {
     if (a >= b) {
         return;
     }
     int m, k, l, r;
     m = rand() % (b - a + 1) + a;
-    k = x[m];
+    k = mas[m];
     l = a - 1;
     r = b + 1;
     while (1) {
         do {
             l = l + 1;
-        } while (x[l] < k);
+        } while (mas[l] < k);
 
         do {
             r = r - 1;
-        } while (x[r] > k);
+        } while (mas[r] > k);
         if (l >= r)
             break;
 
-        std::swap(x[l], x[r]);
+        std::swap(mas[l], mas[r]);
     }
     r = l;
     l = l - 1;
-    quicksort(a, l, x);
-    quicksort(r, b, x);
+    quickSort(a, l, mas);
+    quickSort(r, b, mas);
+}
+
+int binarySearch(int* mas, int s, int n, int k){
+    if(k<mas[s]) return -1;
+    if(k==mas[s]) return 0;
+    if(k>mas[s]) return -1;
+
+    int a = s;
+    int b = n;
+
+    while(a+1<b){
+        int c = (a+b) / 2;
+
+        if(k>mas[c]) a = c;
+        else b = c;
+    }
+
+    if(mas[b] == k) return b;
+    else return -1;
 }
 
 int main() {
     srand(time(0));
-    
 
+    std::vector<int> mas;
 
+    for(int i = 0; i < 10; i++){
+        mas.push_back(rand()%1000);
+    }
+    quickSort(0, mas.size(), mas);
 
+    for (auto& element : mas){
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+
+    mas.emplace(mas.begin()+5, 999);
+    mas.erase(mas.begin()+5);
 
 
     return 0;
