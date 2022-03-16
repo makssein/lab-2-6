ade131055bac3dda809957a5644dcba4fe66f9ba
deekep <MVSein@stud.kantiana.ru>
2022-03-16 22:30:09 +0200

    Revert "Merge branch 'list'"
    This reverts commit e328f4c8a4ea778eb29617371f64c1d271412705.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@ int main() {
     srand(time(0));
     std::list<int> list;
 
-    for(int i = 0; i<10; i++){
+    for(int i = 0; i < 10; i++){
         list.push_back(rand()%1000);
     }
+
+    for (auto c : list)
+        std::cout << " " << c;
+    std::cout << std::endl;
+
     return 0;
 }
