278ac505e3232761afc0e197501550c5d24b311a
deekep <MVSein@stud.kantiana.ru>
2022-03-16 22:29:43 +0200

    add list

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <list>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    srand(time(0));
+    std::list<int> list;
+
+    for(int i = 0; i<10; i++){
+        list.push_back(rand()%1000);
+    }
     return 0;
 }
